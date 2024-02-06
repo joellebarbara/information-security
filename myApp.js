943bf2d225c4bf5702e8cfeffb1f3d4ff07c3e51
Joelle Barbara <joellebarbara@Joelles-MacBook-Pro.local>
2024-02-06 15:55:32 +0200

    xsfil

--- myApp.js
+++ myApp.js
@@ -4,10 +4,8 @@ const app = express();
 
 
 app.use(helmet.hidePoweredBy());
-
-app.use (helmet.frameguard({ action: 'DENY' }));
-
 app.use(helmet.xssFilter());
+app.use (helmet.frameguard({ action: 'DENY' }));
 
 
 
